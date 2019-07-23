d0fb9d756f5984a47d6b4e446ecb6f196cd0ae0c
Brett Fisher <bdf5106@psu.edu>
2019-07-23 00:45:13 -0400

    all my reddit app stuff so far

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+const snoowrap = require('snoowrap');;
+
+//<img [hidden]="img == null || img == 'self'" [ngStyle]="setImageSize()" src="{{ img }}">
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  subredditInput = '';
+  postTitle = '';
+  postText = '';
+  img;
+  url = '';
+  submissionId = '';
+  comments = '';
+
+  width;
+  height;
+
+  setImageSize() {
+    let style = {
+      'width': 'this.width'+'px',
+      'height': 'this.height'+'px'
+    };
+    return style
+  }
+
+  getSubredditData() {
+
+    const r = new snoowrap({
+      userAgent: 'my-app',
+      clientId: '1tQ0hKzdmj18fA',
+      clientSecret: 'ZuMaXl85Kia_6QjpKnnx-G9U5ns',
+      username: 'bdf1221',
+      password: 'RecogXcity23$'
+    })
+
+    var data = r
+      .getSubreddit(this.subredditInput)
+      .getNew({ limit: 1 })
+      .then(r => {
+        console.log(r);
+        this.postTitle = r[0].title;
+        this.postText = r[0].selftext;
+        this.submissionId = r[0].id;
+        this.img = null;
+
+        
+        // if there's an image, show
+        if (r[0]['preview']['images'][0]['source']['url']) {
+          this.img = r[0]['preview']['images'][0]['source']['url'];
+        } else {
+          this.img = null;
+        }
+
+        if (r[0].url) {
+          this.url = r[0].url;
+        } else {
+          this.url = "";
+        }
+      })
+
+      var replies = r
+        .getSubmission(this.submissionId)
+        .expandReplies()
+        .then(r => {
+          console.log(r);
+        })
+
+    console.log(data);
+  }
 }
