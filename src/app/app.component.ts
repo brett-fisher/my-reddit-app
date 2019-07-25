79b2274393970c677554c9a8faba55a674b06148
Brett Fisher <bdf5106@psu.edu>
2019-07-24 22:47:27 -0400

    Finished up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
-const snoowrap = require('snoowrap');;
+const snoowrap = require('snoowrap');
 
-//<img [hidden]="img == null || img == 'self'" [ngStyle]="setImageSize()" src="{{ img }}">
+/*
+  As a quick note...I could probably abstract the logic which gets
+  the reddit data out into another object with methods that I call in
+  the getSubredditData() method...that would be v2 of this. It would 
+  look something like:
+
+    getSubredditData() {
+      var r = new Reddit()
+
+      reddit.getTitle()
+      reddit.getComments()
+    }
+
+    KNOWN BUG: couldnt figure out how to use angular to clear the 
+    comments from the previous post that gets rendered.
+*/
+
 
 @Component({
   selector: 'app-root',
@@ -15,27 +31,20 @@ export class AppComponent {
   img;
   url = '';
   submissionId = '';
-  comments = '';
+  comments = [];
 
   width;
   height;
 
-  setImageSize() {
-    let style = {
-      'width': 'this.width'+'px',
-      'height': 'this.height'+'px'
-    };
-    return style
-  }
-
   getSubredditData() {
+    this.resetComments(this.comments);
 
     const r = new snoowrap({
       userAgent: 'my-app',
-      clientId: '1tQ0hKzdmj18fA',
-      clientSecret: 'ZuMaXl85Kia_6QjpKnnx-G9U5ns',
-      username: 'bdf1221',
-      password: 'RecogXcity23$'
+      clientId: 'PaJVCLl8lIxteA',
+      clientSecret: 'd5jyBGHNnG72HzYUYySQ1XFc-bo',
+      username: 'app4bbot',
+      password: 'Password1'
     })
 
     var data = r
@@ -43,12 +52,12 @@ export class AppComponent {
       .getNew({ limit: 1 })
       .then(r => {
         console.log(r);
+
         this.postTitle = r[0].title;
         this.postText = r[0].selftext;
         this.submissionId = r[0].id;
         this.img = null;
 
-        
         // if there's an image, show
         if (r[0]['preview']['images'][0]['source']['url']) {
           this.img = r[0]['preview']['images'][0]['source']['url'];
@@ -56,20 +65,24 @@ export class AppComponent {
           this.img = null;
         }
 
+        // if there's a url show
         if (r[0].url) {
           this.url = r[0].url;
         } else {
           this.url = "";
         }
-      })
 
-      var replies = r
-        .getSubmission(this.submissionId)
-        .expandReplies(ç)
-        .then(r => {
-          console.log(r);
+        // fetch the comments
+        r[0].comments.fetchAll().then(obj => {
+          for (var i=0; i<obj.length; i++) {
+            this.comments[i] = obj[i].body;
+            this.resetComments(this.comments);
+          }
         })
+      })
+    }
 
-    console.log(data);
-  }
+    resetComments(list) {
+      list = [];
+    }
 }
